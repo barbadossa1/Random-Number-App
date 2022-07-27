061c6a6b474a7213d6ad92a6d83278a65676f10d
Vadim <mr.kavy@yandex.com>
2022-07-27 16:41:40 +0300

    Initial Commit

---
+++ Random Number App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Number App
+//
+//  Created by VK on 27.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
